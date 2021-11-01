73c36a166372a377dcb21205ecc81b44e03b4ed0
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-01 13:03:49 -0700

    commit

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
 
+double pow2(double base)
+{
+	return base * base;
+}
 /**
  */
 void division(complex c1, complex c2, complex *c3)
@@ -9,6 +13,6 @@ void division(complex c1, complex c2, complex *c3)
 	double c = c2.re;
 	double d = c2.im;
 	
-	c3->re = ((a * c) + (b * d)) / (pow(c, 2) + pow(d, 2));
-	c3->im = ((b * c) - (a * d)) / (pow(c, 2) + pow(d, 2));
+	c3->re = ((a * c) + (b * d)) / (pow2(c) + pow2(d));
+	c3->im = ((b * c) - (a * d)) / (pow2(c) + pow2(d));
 }
--- 0x00-math_complex/main.c
+++ 0x00-math_complex/main.c
@@ -10,13 +10,13 @@
 int main(void)
 {
     complex c1, c2, c3;
-    c1.re=1;
-    c1.im=2;
+    c1.re=4;
+    c1.im=3;
     c2.re=2;
-    c2.im=2;
+    c2.im=1;
     display_complex_number(c1);
     display_complex_number(c2);
-    multiplication(c1,c2,&c3);
+    division(c1,c2,&c3);
     display_complex_number(c3);
 	return (0);
 }
