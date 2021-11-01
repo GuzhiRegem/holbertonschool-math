e6957cb8cf84650864b966be3952a1e4051e73bb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-01 15:37:00 -0700

    commit

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -1,5 +1,11 @@
 #include "heron.h"
 
+/**
+ * add_node - adds node to l_list
+ * @head: head of list
+ * @num: num of new obj
+ * Return: void
+ */
 void add_node(t_cell **head, double num)
 {
 	t_cell *new;
@@ -10,13 +16,17 @@ void add_node(t_cell **head, double num)
 	*head = new;
 }
 /**
+ * heron - sequence
+ * @p: sqrt(p)
+ * @x0: first num of iter
+ * Return: l_list of iterations
  */
 t_cell *heron(double p, double x0)
 {
 	double i;
 	t_cell *out = NULL;
 
-	for (i = x0; (i * i) != p; i = 0.5 * (i + (p/i)))
+	for (i = x0; (i * i) != p; i = 0.5 * (i + (p / i)))
 		add_node(&out, i);
 	return (out);
 }
